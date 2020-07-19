c683507e94f93ad9dbafeee093d94dc77ea629e5
mohammadshahed <silentshahed@gmail.com>
2020-07-19 17:58:49 +0600

    settings updated

--- django_project/settings.py
+++ django_project/settings.py
@@ -106,9 +106,11 @@ db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 #DATABASES['default']['CONN_MAX_AGE'] = 500
 
+
+'''
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
+'''
 
 
 '''
