f6550f76347ec23986db9a2e1baa475da897e5a7
mohammadshahed <silentshahed@gmail.com>
2020-07-19 18:34:09 +0600

    finish

--- django_project/settings.py
+++ django_project/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -98,7 +98,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
+
 
 
 
