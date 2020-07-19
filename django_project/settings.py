541213d0bfcef9d4c2e917d802b39b03f9fdc842
mohammadshahed <silentshahed@gmail.com>
2020-07-19 17:38:54 +0600

    settings updated

--- django_project/settings.py
+++ django_project/settings.py
@@ -99,14 +99,18 @@ DATABASES = {
     }
 }
 '''
+
+'''
 import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 #DATABASES['default']['CONN_MAX_AGE'] = 500
-'''
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 '''
+
+
 '''
 DATABASES = {
     'default': {
