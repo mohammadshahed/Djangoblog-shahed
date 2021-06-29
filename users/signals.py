83aba2d9a6665597fd538a27fe4bf47dd803672b
mohammadshahed <silentshahed@gmail.com>
2021-06-29 11:54:53 +0600

    ok

--- users/signals.py
+++ users/signals.py
@@ -1,15 +1,13 @@
+
 from django.db.models.signals import post_save
-from django.contrib.auth.models import User
-from django.dispatch import receiver
 from .models import Profile
+from django.contrib.auth.models import User
 
+def create_profile(sender, **kw):
+    user = kw["instance"]
+    if kw["created"]:
+        profile = Profile()
+        profile.user = user
+        profile.save()
 
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+post_save.connect(create_profile, sender=User)
