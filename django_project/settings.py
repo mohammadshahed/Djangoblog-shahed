6b5e9072e62f332e777d9415ff58f97b384a27b6
mohammadshahed <silentshahed@gmail.com>
2021-07-02 02:16:42 +0600

    ok

--- django_project/settings.py
+++ django_project/settings.py
@@ -8,8 +8,8 @@ SECRET_KEY = '==o(i0@j^1h%zuy#^nly%vf3d3_^n_*sweph_ia*ur%np35li7'
 
 DEBUG = True
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['djangoblog-shahed.herokuapp.com']
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['djangoblog-shahed.herokuapp.com']
 
 
 
@@ -29,7 +29,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
